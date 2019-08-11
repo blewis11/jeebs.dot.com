5fa7e152e598e7e85c4e8f44938b1344799c4418
blewis11 <bejallewis@hotmail.com>
2019-08-11 14:33:55 +0200

    remove some styles to make room for my own

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
          Website coming soon :)
         </p>
